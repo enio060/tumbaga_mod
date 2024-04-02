d5113a38db76c3f6baff1d1957da0df5e175e811
enio060 <kacperbilka1@gmail.com>
2024-04-02 13:20:34 +0200

    Initial commit

---
+++ src/main/java/net/enio060/tumbagamod/TumbagaClient.java
@@ -0,0 +1,10 @@
+package net.enio060.tumbagamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TumbagaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/enio060/tumbagamod/TumbagaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.enio060.tumbagamod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TumbagaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
